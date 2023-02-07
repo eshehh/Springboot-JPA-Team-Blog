1d1b8d38f352cd06b966bde536e00ea747cb30b1
eshehh <qotjsgks1092@naver.com>
2023-02-07 09:17:21 +0900

    환경세팅 완료

---
+++ src/test/java/com/cos/bloh/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.bloh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
