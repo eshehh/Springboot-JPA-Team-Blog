c7debb29f227b60968c5ea6ef8075ac4238ed022
eshehh <qotjsgks1092@naver.com>
2023-02-10 16:49:10 +0900

    230210 7교시

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -1,10 +1,22 @@
 package com.cos.blog.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.model.OAuthToken;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.Data;
 
@@ -25,8 +37,54 @@ public class UserController {
 		return "user/loginForm";
 	}
 	
+	@GetMapping("auth/kakao/callback")
+	public @ResponseBody String kakaoCallback(String code) { //Data를 리턴해주는 컨트롤러 함수
+		
+		// POST방식으로 key=value 데이터를 요청 (카카오쪽으로)
+		RestTemplate rt = new RestTemplate();
+		
+		//HttpHeader 오브젝트 생성
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		//HttpBody 오브젝트 생성
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "ab6972353e90e5bf1c8612ec0f138ec6");
+		params.add("redirect_uri", "http://localhost:8000/auth/kakao/callback");
+		params.add("code", code);
+		
+		// HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+				new HttpEntity<>(params, headers);
+		
+		// Http 요청하기 - POST방식으로 - 그리고 response 변수의 응답 받음.
+		ResponseEntity<String> response = rt.exchange(
+					"https://kauth.kakao.com/oauth/token",
+					HttpMethod.POST,
+					kakaoTokenRequest,
+					String.class
+				);
+		
+		// Gson, Json Simple, ObjectMapper
+		ObjectMapper objectMapper = new ObjectMapper();
+		OAuthToken oauthToken = null;
+		try {
+			 oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("카카오 엑세스 토큰 :"+oauthToken.getAccess_token());
+		
+		return response.getBody();
+		
+	}
+	
 	@GetMapping("/user/updateForm")
-	public String updateForm( @AuthenticationPrincipal PrincipalDetail principal ) {
+	public String updateForm() {
 		return "user/updateForm";
 	}
 	
--- src/main/webapp/WEB-INF/views/user/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/user/loginForm.jsp
@@ -19,6 +19,7 @@
   
   
   <button id="btn-login" class="btn btn-primary">로그인</button>
+  <a href="https://kauth.kakao.com/oauth/authorize?client_id=ab6972353e90e5bf1c8612ec0f138ec6&redirect_uri=http://localhost:8000/auth/kakao/callback&response_type=code"><img height="38px" src="/image/kakao_login_button.png"/></a>
 </form>
 	
 
